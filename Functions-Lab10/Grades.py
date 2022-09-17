b7e2dcf7fe328661a5a592baf3bafcc5ed05b3f4
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-09-17 13:54:44 +0300

    Add files via upload

---
+++ Functions-Lab10/Grades.py
@@ -0,0 +1,16 @@
+grade = float(input())
+
+def receives_a_grade(grade):
+    if 2.00 <= grade <= 2.99:
+        return'Fail'
+    elif 3.00 <= grade <= 3.49:
+        return 'Poor'
+    elif 3.50 <= grade <= 4.49:
+        return 'Good'
+    elif 4.50 <= grade <= 5.49:
+        return'Very Good'
+    elif 5.50 <= grade <= 6.00:
+        return 'Excellent'
+
+
+print(receives_a_grade(grade))
