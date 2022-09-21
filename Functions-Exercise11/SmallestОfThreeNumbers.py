1a3ab0fded87af96779bbcc3ce2cf9324e5da9ec
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-09-21 17:23:31 +0300

    Add files via upload

---
+++ Functions-Exercise11/SmallestОfThreeNumbers.py
@@ -0,0 +1,12 @@
+def smallest(a, b, c):
+    return min(a, b, c)
+
+
+first_int = int(input())
+second_int = int(input())
+third_int = int(input())
+
+result = smallest(first_int, second_int, third_int)
+
+
+print(result)
