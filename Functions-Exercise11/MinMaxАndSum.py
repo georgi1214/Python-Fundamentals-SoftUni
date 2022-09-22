5b6266b4709e7f2f939f8ca806e20e96c855da7f
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-09-22 16:07:47 +0300

    Add files via upload

---
+++ Functions-Exercise11/MinMaxАndSum.py
@@ -0,0 +1,9 @@
+numbers = list(map(int, input().split(" ")))
+
+min_numbers = min(numbers)
+max_numbers = max(numbers)
+sum_numbers = sum(numbers)
+
+print(f"The minimum number is {min_numbers}")
+print(f"The maximum number is {max_numbers}")
+print(f"The sum number is: {sum_numbers}")
