d1323d8589f1d5cbbf7559e16b2307a838dea08c
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-09-30 19:41:27 +0300

    Add files via upload

---
+++ ListsAdvanced-Exercise15/WhichAreIn.py
@@ -0,0 +1,11 @@
+first_list = input().split(', ')
+second_list = input().split(', ')
+substring = []
+
+for first_word in first_list:
+    for second_word in second_list:
+        if first_word in second_word and not first_word in substring:
+            substring.append(first_word)
+
+
+print(substring)
