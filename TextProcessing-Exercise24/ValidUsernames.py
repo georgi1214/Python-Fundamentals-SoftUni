af9e97fa3f9e80648934c867927d7480fdbe1f3a
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-10-21 17:59:43 +0300

    Add files via upload

---
+++ TextProcessing-Exercise24/ValidUsernames.py
@@ -0,0 +1,12 @@
+usernames = input().split(", ")
+for username in usernames:
+    username_is_valid = True
+    if not 3 <= len(username) <= 16:
+        username_is_valid = False
+    for character in username:
+        if not(character.isalnum() or character == "-" or character == "_"):
+            username_is_valid = False
+    if ' ' in username:
+        username_is_valid = False
+    if username_is_valid: #if username_is_valid == True
+        print(username)
