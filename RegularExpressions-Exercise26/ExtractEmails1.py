c3b37171997d093db63a0434dbdcae03263012b0
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-10-31 17:50:45 +0200

    Add files via upload

---
+++ RegularExpressions-Exercise26/ExtractEmails1.py
@@ -0,0 +1,8 @@
+import re
+
+text = input()
+
+pattern = r"(^|(?<=\s))[a-zA-Z0-9]+[\._-]?[a-zA-Z0-9]+@[a-z]+-?[a-z]+(\.[a-z]+)+"
+
+valids = [el.group() for el in re.finditer(pattern, text)]
+print('\n'.join(valids))
